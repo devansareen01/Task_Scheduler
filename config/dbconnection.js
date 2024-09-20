5d702d94cedd8bc5765eada0977219c37b27df5d
devansareen01 <devansareen6@gmail.com>
2024-09-20 14:27:41 +0530

    changes done

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -2,9 +2,6 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();  // Make sure dotenv is set up for local development
 
 
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
-
 const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:Devan@localhost:5432/task_scheduler', {
     dialect: 'postgres',
     logging: false,
